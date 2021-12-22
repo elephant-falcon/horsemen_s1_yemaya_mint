0ef29b5e9d97064c739862e1b8558b9cef364a8b
Jani Momolu Anderson <ja@janianderson.com>
2021-12-22 10:38:33 -0600

    Installed Atomize templates

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./styles/reset.css";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
+
+const debug =
+process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+// 2. Provide the engine to the app
+// debug engine needs inlined source maps
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider value={engine} debug={debug} debugAfterHydration store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
